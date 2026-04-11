af51ba836ed89478b175f1a42df31dbca5145b90
jones <dummyjones254@gmail.com>
2026-04-11 11:00:29 +0300

    adding return

---
+++ practice_exercises/print_asterisks_rectangle.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+//print an asterisks rectangle of 10(length) and 5(width)
+int main() {
+    printf("**********\n**********\n**********\n**********\n**********\n");
+    return 0;
+}
---
+++ practice_exercises/print_year_of_birth.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("My year of birth is: %d\n", 2026-35);
+    return 0;
+}
